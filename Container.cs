6207d1701b05ad07d9ba834eb91f3ed6262b26a9
s27492-PR <s27492@pjwstk.edu.pl>
2025-03-29 22:04:45 +0100

    Added Container + ship classes

---
+++ Container.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Container
+{
+
+private static int _counterForSerialNum = 1; // Counter for unique serial numbers
+public double LoadWeight { get; set; } // kg
+public double Height { get; set; }     // cm
+public double Weight { get; set; }     // kg
+public double Depth { get; set; }      // cm
+public double MaxLoad { get; set; }    // kg
+public string SerialNum { get; private set; } // generated later
+
+    public Container(double loadWeight, double height, double weight, double depth, double maxLoad)
+    {
+    LoadWeight = loadWeight;
+    Height = height;
+    Weight = weight;
+    Depth = depth;
+    MaxLoad = maxLoad;
+    SerialNum = GenerateSerialNumber();
+    
+    }
+
+    public virtual void Load(double weight) 
+    {
+        if (this.LoadWeight + weight > this.MaxLoad) 
+        {
+            throw new OverfillException("Load exceeds container capacity.");
+        }
+        this.LoadWeight += weight;
+    }
+    public virtual void Unload()
+    {
+        LoadWeight = 0;
+    }
+
+    // Method for generating custom/unique container serial number
+    private string GenerateSerialNumber()
+    {
+        string containerType = this switch
+        {
+            LiquidContainer => "L",
+            GasContainer => "G",
+            RefrigeratedContainer => "C",
+            _ => "Default"
+        };
+
+        return $"KON-{containerType}-{_counterForSerialNum++}";
+    }
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Serial Number: {SerialNum}");
+        Console.WriteLine($"Type: {GetType().Name}");
+        Console.WriteLine($"Load Weight: {LoadWeight} kg");
+        Console.WriteLine($"Container Weight: {Weight} kg");
+        Console.WriteLine($"Max Load: {MaxLoad} kg");
+        Console.WriteLine($"Dimensions: {Height}cm x {Depth}cm");
+    }
+
+
+}
---
+++ Ship.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+public class Ship
+{
+    public List<Container> Containers;
+    public double MaxSpeed;
+    public int MaxContainerNumber;
+    public double MaxWeight;
+
+
+    public Ship(List<Container> containers, double maxSpeed, int maxContainerNumber, double maxWeight)
+    {
+        Containers = containers;
+        MaxSpeed = maxSpeed;
+        MaxContainerNumber = maxContainerNumber;
+        MaxWeight = maxWeight;
+    }
+
+    public void AddContainer(Container container)
+    {
+        double totalWeight = this.Containers.Sum(x => x.LoadWeight + x.Weight);
+
+        if (Containers.Count >= this.MaxContainerNumber)
+        {
+            throw new InvalidOperationException("Ship is full - can't add more containers");
+        }
+
+        if (totalWeight + container.LoadWeight + container.Weight > this.MaxWeight * 1000)
+        {
+            throw new InvalidOperationException("Weight limit exceeded - can't add container");
+        }
+
+        Containers.Add(container);
+    }
+    public void RemoveContainer(string serialNumber)
+    {
+        Container conToBeRemoved = this.Containers.FirstOrDefault(x => x.SerialNum == serialNumber);
+        if (conToBeRemoved == null)
+        {
+            throw new InvalidOperationException("Container not found");
+        }
+        Containers.Remove(conToBeRemoved);
+    }
+
+    public void ReplaceContainer(Container container, string serialNumber)
+    {
+        Container conToBeReplaced = this.Containers.FirstOrDefault(x => x.SerialNum == serialNumber);
+        if (conToBeReplaced == null)
+        {
+            throw new InvalidOperationException("Container not found");
+        }
+        double totalWeight = Containers.Sum(x => x.LoadWeight + x.Weight) - (conToBeReplaced.LoadWeight + conToBeReplaced.Weight) + (container.LoadWeight + container.Weight);
+
+        if (totalWeight > MaxWeight * 1000)
+        {
+            throw new InvalidOperationException("New container exceeds weight limit.");
+        }
+        Containers.Remove(conToBeReplaced);
+        Containers.Add(container);
+
+    }
+    public void TransferContainer(Ship targetShip, string serialNum)
+    {
+        Container container = Containers.FirstOrDefault(x => x.SerialNum == serialNum);
+        if (container == null)
+        {
+            throw new InvalidOperationException("Container not found.");
+        }
+
+        RemoveContainer(serialNum);
+        targetShip.AddContainer(container);
+    }
+    public void PrintShipInfo()
+    {
+        Console.WriteLine($"Ship Info: Speed: {MaxSpeed} knots, Max Containers: {MaxContainerNumber}, Max Weight: {MaxWeight} tons");
+        Console.WriteLine($"Current containers: {Containers.Count}");
+
+        foreach (var container in Containers)
+        {
+            Console.WriteLine($"- {container.SerialNum} ({container.GetType().Name}) | Load: {container.LoadWeight} kg");
+        }
+    }
+}
